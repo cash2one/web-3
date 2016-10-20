84048b019ad25b6a99ff8ad03af82cd0595afd3a
zdd135github <2271404280@qq.com>
2016-10-20 17:38:41 +0800

    new

--- MySQL/sql_script/case.sql
+++ MySQL/sql_script/case.sql
@@ -1,6 +1,6 @@
 /*
 * @Date:   2016-09-26 15:17:39
-* @Last Modified time: 2016-10-11 17:50:57
+* @Last Modified time: 2016-10-20 17:36:12
 */
 
 -- 执行sql脚本，可以有2种方法:
@@ -8,24 +8,7 @@
 -- mysql>source $path\***.sql (注意路径不用加引号) 或者 \. $path\***.sql (注意路径不用加引号)
 
 USE `crm`;
--- SUM IF语句
-select
-    grd.id,
-    grd.accendant_id,
-    grd.accendant_deptname,
-    grd.rent_type,
-    FROM_UNIXTIME(grd.modify_at) AS modifyAtStr,
-    grd.rent,
-    SUM(IF(grd.rent_type=1, grd.rent, 0)) AS baseRent,
-    SUM(IF(grd.rent_type=2, grd.rent, 0)) AS yejiRent,
-    SUM(IF(grd.rent_type=3, grd.rent, 0)) AS jiangliRent,
-    SUM(IF(grd.rent_type=4, grd.rent, 0)) AS chengfaRent,
-    ABS(SUM(IF(grd.rent_type=5, grd.rent, 0))) AS xiaohaoRent,
-    SUM(IF(rent_type=5,1,0)) AS kehuCount,  -- 统计rent_type为5的
-    SUM(grd.rent) AS totalRent
-from gw_rent_detail grd
-WHERE grd.accendant_deptname != ""
-GROUP BY grd.accendant_id;
+
 
 -- CASE语句
 select
---
+++ MySQL/sql_script/str.sql
@@ -0,0 +1,15 @@
+/*
+* @Date:   2016-10-20 17:30:30
+* @Last Modified time: 2016-10-20 17:36:38
+*/
+USE `crm`;
+-- 2016 05 20 09 17 56
+SELECT
+    LEFT(o4.`StartDate`,4),
+    SUBSTRING(o4.`StartDate`,5,2),
+    SUBSTRING(o4.`StartDate`,7,2),
+    SUBSTRING(o4.`StartDate`,9,2),
+    SUBSTRING(o4.`StartDate`,11,2),
+    RIGHT(o4.`StartDate`,2),
+    FROM_UNIXTIME(o4.modify_at) AS modifyAtStr
+FROM order_400calllog o4;
