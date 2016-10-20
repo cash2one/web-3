2473b5c09c126ad1c732ec6b67170c3a0fa41ca9
zdd135github <2271404280@qq.com>
2016-10-20 17:46:06 +0800

    new

--- MySQL/sql_script/str.sql
+++ MySQL/sql_script/str.sql
@@ -1,6 +1,6 @@
 /*
 * @Date:   2016-10-20 17:30:30
-* @Last Modified time: 2016-10-20 17:36:38
+* @Last Modified time: 2016-10-20 17:41:44
 */
 USE `crm`;
 -- 2016 05 20 09 17 56
@@ -12,4 +12,20 @@ SELECT
     SUBSTRING(o4.`StartDate`,11,2),
     RIGHT(o4.`StartDate`,2),
     FROM_UNIXTIME(o4.modify_at) AS modifyAtStr
+FROM order_400calllog o4;
+
+SELECT
+    CONCAT(
+        LEFT(o4.`StartDate`,4),
+        "-",
+        SUBSTRING(o4.`StartDate`,5,2),
+        "-",
+        SUBSTRING(o4.`StartDate`,7,2),
+        " ",
+        SUBSTRING(o4.`StartDate`,9,2),
+        ":",
+        SUBSTRING(o4.`StartDate`,11,2),
+        ":",
+        RIGHT(o4.`StartDate`,2)
+        ) AS StartDate
 FROM order_400calllog o4;
