1be4e7830ae33f0045013a252e0139be645bdd95
zdd135github <2271404280@qq.com>
2016-12-23 09:36:00 +0800

    2016-12-23 09:36:00

--- frame_django/azure/forward/authentication_modify/test.py
+++ frame_django/azure/forward/authentication_modify/test.py
@@ -5,3 +5,5 @@
 # 进行用户验证的python类的路径————调用django.contrib.auth.authenticate()会一一尝试
 # 如果用户名和密码在多个后台中都是合法的，Django 将在第一个匹配成功后停止处理
 from django.contrib.auth.backends import ModelBackend
+
+from django.contrib.sessions.middleware import SessionMiddleware
