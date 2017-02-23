242f0f1c41dbe3e4af4eb25d0a43448139190a8e
zdd135github <2271404280@qq.com>
2017-02-23 19:19:59 +0800

    2017-02-23 19:19:59

--- MySQL/union.sql
+++ MySQL/union.sql
@@ -1,9 +1,10 @@
 /*
 * @Date:   2017-02-16 15:20:48
-* @Last Modified time: 2017-02-16 15:20:57
+* @Last Modified time: 2017-02-23 16:35:54
 */
-    <select id="selectCustomerByUserIdListPage" resultMap="opencustomerMap" parameterType="com.uban.crm.domain.open.OpenCustomer">
-        SELECT
+select * from(
+    (
+    SELECT
         oc.id AS openCustomerId,
         oc.`customer_name`,
         oc.city_id,
@@ -16,18 +17,17 @@
         oc.`order_area_end`,
         oc.`order_price_begin`,
         oc.`order_price_end`,
+        '' AS tag,
         '未接单' AS isAccept,
-        oc.create_at
-        FROM `open_customer_send` ocs
-        LEFT JOIN `open_customer` oc ON ocs.`customer_id`=oc.`id`
-        <trim prefix="where" prefixOverrides="and|or">
-            <if test="vo.userId != null">
-                and ocs.user_id = #{vo.userId}
-            </if>
-        </trim>
-        UNION
-        (
-        select
+        oc.create_at,
+        ocs.end_at
+    FROM `open_customer_send` ocs
+    LEFT JOIN `open_customer` oc ON ocs.`customer_id`=oc.`id`
+    where ocs.user_id = 666753
+    )
+    UNION
+    (
+    select
         oc.id AS openCustomerId,
         oc.`customer_name`,
         oc.city_id,
@@ -42,14 +42,14 @@
         oc.`order_price_end`,
         ouc.tag,
         case ouc.`systemresource` when 0 then '接单' when 1 then '私客' else '' end AS isAccept,
-        oc.create_at
-        from `open_user_customer` ouc
-        left join `open_customer` oc on ouc.`open_customer_id`=oc.`id`
-        <trim prefix="where" prefixOverrides="and|or">
-            <if test="vo.userId != null">
-                and ouc.user_id = #{vo.userId}
-            </if>
-        </trim>
-        order by oc.create_at DESC
-        )
-    </select>
+        oc.create_at,
+        0 AS end_at
+    from `open_user_customer` ouc
+    left join `open_customer` oc on ouc.`open_customer_id`=oc.`id`
+    where ouc.user_id = 666753
+    order by oc.create_at DESC
+    )
+)
+AS temp
+-- group by写在最外层才能生效
+group by temp.end_at desc, temp.create_at desc
