998cc086dffb19946d352330368ccf83a7655527
zdd135github <2271404280@qq.com>
2017-03-09 11:06:15 +0800

    2017-03-09 11:06:14

--- py_spider/spider_modules/demo_scrapy/run.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date:   2017-02-23 10:07:03
-# @Last Modified time: 2017-02-23 13:23:49
-#
-import os
-os.system('scrapy crawl job & pause')
--- py_spider/spider_modules/module_cookie.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Date:   2016-11-28 09:23:51
-# @Last Modified time: 2017-03-06 11:42:08
-import Cookie
-# SimpleCookie(BaseCookie(dict))
-c = Cookie.SimpleCookie()
-c['key1'] = 'value1'
-print c
