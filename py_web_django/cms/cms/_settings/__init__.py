1c70cb952a84b6011d11dbe66e6bbaddc5a23b66
zdd135github <2271404280@qq.com>
2017-03-22 22:52:37 +0800

    2017-03-22 22:52:36

--- py_web_django/cms/cms/_settings/__init__.py
+++ py_web_django/cms/cms/_settings/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Date:   2016-12-22 21:14:08
 # @Last Modified time: 2017-01-02 21:23:10
-#
-# 拆分settings
-# 文件夹名称不能与settings入口文件————settings.py重名
+"""
+拆分settings
+文件夹名称不能与settings入口文件————settings.py重名
+"""
